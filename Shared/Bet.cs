f7c200d260c45eb0993671ebbf778faaa17e4fa2
Doug Jih <doug.jih@gmail.com>
2021-04-17 13:54:52 -0400

    Add initial sketch of BetPairings page (does not work yet)

---
+++ Shared/Bet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CloudClassProject.Shared
+{
+    public class Bet
+    {
+        public string Bookmaker { get; set; }
+        public string OutcomeDescription { get; set; }
+        public int Odds { get; set; }
+        public Decimal Wager { get; set; }
+        public Decimal Offset { get; set; }
+    }
+}
---
+++ Shared/BetPairing.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudClassProject.Shared
+{
+    public class BetPairing
+    {
+        public Bet Bet1 { get; set; }
+        public Bet Bet2 { get; set; }
+    }
+}
