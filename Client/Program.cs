83d40b81fa178680956be32ca8de96421842f0f7
Doug Jih <doug.jih@gmail.com>
2021-05-01 13:00:26 -0400

    Transplant the AD B2C template back into Client
    Hoping to get AD B2C to work again, while still keeping Azure Functions
    working.
    
    I got the Azure Functions to keep working in this commit. The following
    turned out to be a key part that I needed to remained in Program.cs:
    
    var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
    builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace BlazorApp.Client
 {
@@ -11,10 +15,15 @@ namespace BlazorApp.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
             builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+            });
 
             await builder.Build().RunAsync();
         }
